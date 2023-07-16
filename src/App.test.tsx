c931ae1dd9f58b76d10137230789d7cdea0830dc
fkdlsl9088 <fkdlsl9088@naver.com>
2023-07-16 21:42:47 +0900

    :fire: Test code refactoring

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Pagination from "./App";
 import {
   fireEvent,
@@ -10,21 +9,30 @@ import "@testing-library/jest-dom/extend-expect";
 
 const PAGE_NUMBER_TEST_ID = "page-number";
 
+const renderPageNumbers = (itemsPerPage?: number) => {
+  render(
+    <Pagination
+      totalItems={6}
+      itemsPerPage={itemsPerPage ?? 3}
+      pageNumberTestId={PAGE_NUMBER_TEST_ID}
+    />
+  );
+
+  const prevButton = screen.getByText(/previous/i);
+  const nextButton = screen.getByText(/next/i);
+
+  return {
+    prevButton,
+    nextButton,
+  };
+};
+
 describe("Pagination", () => {
   test("Pagination 컴포넌트 렌더링", () => {
-    render(
-      <Pagination
-        totalItems={6}
-        itemsPerPage={3}
-        pageNumberTestId={PAGE_NUMBER_TEST_ID}
-      />
-    );
+    const { prevButton, nextButton } = renderPageNumbers();
 
     // NOTE: assert (내가 예상하는 결과값이 잘 나왔는지 확인하는 코드 )
 
-    // 해당하는 text 내용이 없다면 error 발생
-    const prevButton = screen.getByText(/previous/i);
-    const nextButton = screen.getByText(/next/i);
     // 해당하는 class가 없다면 error 발생);
     expect(prevButton).toHaveClass("disabled");
     expect(nextButton).not.toHaveClass("disabled");
@@ -38,14 +46,7 @@ describe("Pagination", () => {
   });
 
   test("첫번째 페이지에서는 이전 페이지로 돌아갈 수 없음", () => {
-    render(
-      <Pagination
-        totalItems={6}
-        itemsPerPage={3}
-        pageNumberTestId={PAGE_NUMBER_TEST_ID}
-      />
-    );
-    const prevButton = screen.getByText(/previous/i);
+    const { prevButton } = renderPageNumbers(1);
 
     fireEvent.click(prevButton);
 
@@ -53,15 +54,7 @@ describe("Pagination", () => {
   });
 
   test("중간 페이지에서는 이전, 다음 페이지로 이동할 수 있음", () => {
-    render(
-      <Pagination
-        totalItems={6}
-        itemsPerPage={2}
-        pageNumberTestId={PAGE_NUMBER_TEST_ID}
-      />
-    );
-    const prevButton = screen.getByText(/previous/i);
-    const nextButton = screen.getByText(/next/i);
+    const { prevButton, nextButton } = renderPageNumbers(2);
 
     fireEvent.click(nextButton);
 
@@ -70,14 +63,7 @@ describe("Pagination", () => {
   });
 
   test("마지막 페이지에서는 다음 버튼을 클릭했을 때 다음 페이지로 이동할 수 없음", () => {
-    render(
-      <Pagination
-        totalItems={6}
-        itemsPerPage={3}
-        pageNumberTestId={PAGE_NUMBER_TEST_ID}
-      />
-    );
-    const nextButton = screen.getByText(/next/i);
+    const { nextButton } = renderPageNumbers();
 
     fireEvent.click(nextButton);
 
