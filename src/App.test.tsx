789394c0c54268014495f89ec88996d9b55105de
fkdlsl9088 <fkdlsl9088@naver.com>
2023-07-16 21:29:07 +0900

    :fire: React simple test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,86 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import Pagination from "./App";
+import {
+  fireEvent,
+  getAllByTestId,
+  render,
+  screen,
+} from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const PAGE_NUMBER_TEST_ID = "page-number";
+
+describe("Pagination", () => {
+  test("Pagination 컴포넌트 렌더링", () => {
+    render(
+      <Pagination
+        totalItems={6}
+        itemsPerPage={3}
+        pageNumberTestId={PAGE_NUMBER_TEST_ID}
+      />
+    );
+
+    // NOTE: assert (내가 예상하는 결과값이 잘 나왔는지 확인하는 코드 )
+
+    // 해당하는 text 내용이 없다면 error 발생
+    const prevButton = screen.getByText(/previous/i);
+    const nextButton = screen.getByText(/next/i);
+    // 해당하는 class가 없다면 error 발생);
+    expect(prevButton).toHaveClass("disabled");
+    expect(nextButton).not.toHaveClass("disabled");
+
+    // NOTE: testId에 해당하는 엘리먼트 접근
+    const pageNumbers = screen.getAllByTestId(PAGE_NUMBER_TEST_ID);
+
+    pageNumbers.forEach((pageNumber, index) => {
+      expect(pageNumber).toHaveTextContent(`${index + 1}`);
+    });
+  });
+
+  test("첫번째 페이지에서는 이전 페이지로 돌아갈 수 없음", () => {
+    render(
+      <Pagination
+        totalItems={6}
+        itemsPerPage={3}
+        pageNumberTestId={PAGE_NUMBER_TEST_ID}
+      />
+    );
+    const prevButton = screen.getByText(/previous/i);
+
+    fireEvent.click(prevButton);
+
+    expect(prevButton).toHaveClass("disabled");
+  });
+
+  test("중간 페이지에서는 이전, 다음 페이지로 이동할 수 있음", () => {
+    render(
+      <Pagination
+        totalItems={6}
+        itemsPerPage={2}
+        pageNumberTestId={PAGE_NUMBER_TEST_ID}
+      />
+    );
+    const prevButton = screen.getByText(/previous/i);
+    const nextButton = screen.getByText(/next/i);
+
+    fireEvent.click(nextButton);
+
+    expect(prevButton).not.toHaveClass("disabled");
+    expect(nextButton).not.toHaveClass("disabled");
+  });
+
+  test("마지막 페이지에서는 다음 버튼을 클릭했을 때 다음 페이지로 이동할 수 없음", () => {
+    render(
+      <Pagination
+        totalItems={6}
+        itemsPerPage={3}
+        pageNumberTestId={PAGE_NUMBER_TEST_ID}
+      />
+    );
+    const nextButton = screen.getByText(/next/i);
+
+    fireEvent.click(nextButton);
+
+    expect(nextButton).toHaveClass("disabled");
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default function App({
+  totalItems,
+  itemsPerPage,
+  pageNumberTestId,
+}: {
+  totalItems: number;
+  itemsPerPage: number;
+  pageNumberTestId?: string;
+}) {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handleClick = (number: number) => {
+    setCurrentPage(number);
+  };
+
+  const handlePrevClick = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (currentPage < pageNumbers.length) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const renderPageNumbers = pageNumbers.map((number) => {
+    return (
+      <li key={number}>
+        <span
+          data-testid={pageNumberTestId}
+          className={currentPage === number ? "active" : undefined}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {number}
+        </span>
+      </li>
+    );
+  });
+
+  return (
+    <nav>
+      <ul className="pagination">
+        <li>
+          <a
+            href="#"
+            onClick={handlePrevClick}
+            className={currentPage === 1 ? "disabled" : undefined}
+          >
+            Previous
+          </a>
+        </li>
+        {renderPageNumbers}
+        <li>
+          <a
+            href="#"
+            onClick={handleNextClick}
+            className={
+              currentPage === pageNumbers.length ? "disabled" : undefined
+            }
+          >
+            Next
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
-
-export default App;
